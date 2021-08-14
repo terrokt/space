745adbdb9da8f470556707ef4a340534b368529c
terrokt <terrokt@outlook.com>
2021-08-15 01:14:34 +0300

    fsdf

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="slider arrow">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="slider arrow">',
+    });
 
+    $('.menu-btn').on('click', function () {
+        $('.menu__list').toggleClass('active');
+    })
 });
