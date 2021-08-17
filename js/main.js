d9c6b3d64b18b578442ce4dbfa1a61acb5c35fcc
terrokt <terrokt@outlook.com>
2021-08-17 19:40:41 +0300

    сделал адаптив

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="slider arrow">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="slider arrow">',
+        responsive: [
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: false
+              }
+            },
+            
+          ]
     });
 
     $('.menu-btn').on('click', function () {
